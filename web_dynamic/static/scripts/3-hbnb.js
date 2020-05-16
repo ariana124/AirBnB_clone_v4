8701b6969fb9a8c1fbfbc03667c043a51d46f432
Tr3v1n4t0r <813@holbertonschool.com>
2020-05-16 14:08:30 -0700

    added user get

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+  const amen = {};
+  $('input:checkbox').click(function () {
+    $(this).each(function () {
+      if (this.checked) {
+	amen[$(this).data('id')] = $(this).data('name');
+      } else {
+	delete amen[$(this).data('id')];
+      }
+    ]);
+    if (Object.values(amen).length > 0) {
+      $('.amenities h4').text(object.values(amen).join(', '));
+    } else {
+      $('.amenities h4').html('&nbsp');
+    }
+  });
+});
+
+$.get('http://0.0.0.0:5001/api/v1/status/', function (data, status) {
+  console.log(data);
+  if (data.status === 'OK') {
+    $('DIV#api_status').addClass('available');
+  } else {
+    $('DIV#api_status').removeClass('available');
+  }
+});
+
+const users = {};
+$getJSON('http://0.0.0.0:5001/api/v1/users', function (data) {
+  for (const user of data) {
+    users[user.id] = user.first_name + ' ' + user.last_name;
+  }
+});
